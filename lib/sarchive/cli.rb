67eae5dc68cc842b32a072340d6c792f86bbc3ef
hogesuke <miyado@gmail.com>
2015-05-24 03:18:18 +0900

    initコマンドを作成

--- lib/sarchive/cli.rb
+++ lib/sarchive/cli.rb
@@ -9,6 +9,31 @@ module Sarchive
     desc 'init', '設定ファイルを作成します'
     option :path, :type => :string, :default => './sarchive.config.yml', :banner => '作成する設定ファイルのパス'
     def init()
+      dest = File.expand_path(options[:path])
+
+      STDOUT.print("設定ファイルを作成します\n")
+
+      operation = 'create'
+
+      if File.exist?(dest)
+        STDOUT.print('すでに設定ファイルが存在します。上書きしますか？(y/n)')
+        input = STDIN.gets.chomp
+
+        case input
+          when /^y(es)?$/i
+            operation = 'overwrite'
+          else
+            operation = 'skip'
+        end
+      end
+
+      STDOUT.print("[#{operation}] #{dest}")
+
+      unless operation == 'skip'
+        FileUtils.cp(File.expand_path('../sarchive.config.yml.example', __FILE__), dest)
+      end
+
+      exit(true)
     end
 
     desc 'exec', 'ディスクをアーカイブします'
