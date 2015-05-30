d4c0f933c19df227bc781fda3adf93efaaa6bb3d
hogesuke <miyado@gmail.com>
2015-05-31 00:55:40 +0900

    ログメッセージの修正

--- lib/sarchive/cli.rb
+++ lib/sarchive/cli.rb
@@ -55,6 +55,8 @@ module Sarchive
 
       logger      = PLogger.new(File.expand_path(log_path), File.expand_path(err_log_path))
 
+      logger.info("Sarchive start.")
+
       unless token and secret
         logger.error('tokenまたはsecretが設定されていません')
         exit(false)
@@ -92,20 +94,22 @@ module Sarchive
           next
         end
 
-        logger.info("ターゲット[zone=#{zone}]")
 
         sacloud.set_zone(zone)
 
         disk_ids.each do |disk_id|
+
+          logger.info("Target zone=[#{zone}] disk_id=[#{disk_id}]")
+
           # 作成パート
-          logger.info("アーカイブ作成開始 ディスク[id=#{disk_id}]")
+          logger.info("Making...")
           archive = sacloud.create_archive(disk_id)
 
           unless archive
             next
           end
 
-          logger.info("アーカイブ作成終了 ディスク[id=#{disk_id}]")
+          logger.info("Making success! archive_id=[#{archive.id}]")
 
           # 削除パート
           if auto_delete['enable']
@@ -127,20 +131,20 @@ module Sarchive
 
             if counts
 
-              logger.info("アーカイブ削除開始 ディスク[id=#{disk_id}] 削除基準[counts=#{counts}]")
+              logger.info("Removing... threshold_counts=[#{counts}]")
 
               if counts < sorted_items.length
                 (sorted_items.length - counts).times do |i|
                   ret = sacloud.delete_archive(sorted_items[i][:archive].id)
-                  logger.info("アーカイブ[id=#{ret.id}]の削除に成功しました") if ret
+                  logger.info("Removing success! archive_id=[#{ret.id}]") if ret
                 end
+              else
+                logger.info("A target doesn't exist.")
               end
-
-              logger.info("アーカイブ削除終了 ディスク[id=#{disk_id}] 削除基準[counts=#{counts}]")
 
             elsif hours
 
-              logger.info("アーカイブ削除開始 ディスク[id=#{disk_id}] 削除基準[hours=#{hours}]")
+              logger.info("Removing... threshold_hours=[#{hours}]")
 
               sorted_items.each do |a|
                 at = a[:created_at]
@@ -151,17 +155,17 @@ module Sarchive
 
                 if time < Time.now
                   ret = sacloud.delete_archive(a[:archive].id)
-                  logger.info("アーカイブ[id=#{ret.id}]の削除に成功しました") if ret
+                  logger.info("Removing success! archive_id=[#{ret.id}]") if ret
+                else
+                  logger.info("A target doesn't exist.")
                 end
               end
-
-              logger.info("アーカイブ削除終了 ディスク[id=#{disk_id}] 削除基準[hours=#{hours}]")
-
             end
           end
         end
       end
 
+      logger.info("Sarchive end.")
       exit (true)
     end
   end
